b9b3b8348a5253f6c2a9781be828a547b09f1cf6
ons <elfekihons@gmail.com>
2024-11-22 16:40:17 +0100

    testcommit

--- miniprojet/Form1.cs
+++ miniprojet/Form1.cs
@@ -16,5 +16,11 @@ namespace miniprojet
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bouton cliqué !");
+
+        }
     }
 }
